759fdd621e7a03a6c75c61c7cbe9140bf8a8b5ea
ShTair <git@vsh.jp>
2025-02-06 13:24:24 +0900

    ログと出力を追加

---
+++ Actions.Utils/Log.cs
@@ -0,0 +1,55 @@
+﻿namespace Actions.Utils;
+
+public class Log
+{
+    // https://docs.github.com/ja/enterprise-cloud@latest/actions/writing-workflows/choosing-what-your-workflow-does/workflow-commands-for-github-actions
+
+    public static IDisposable StartGroup(string title) => new AreaCommand("group", "endgroup", title);
+
+    public static void Debug(string message)
+    {
+        Console.WriteLine($"::debug::{message}");
+    }
+
+    public static void Notice(string message, string? title)
+    {
+        Console.WriteLine($"::notice title={title}::{message}");
+    }
+
+    public static void Warning(string message, string? title)
+    {
+        Console.WriteLine($"::warning title={title}::{message}");
+    }
+
+    public static void Error(string message, string? title)
+    {
+        Console.WriteLine($"::error title={title}::{message}");
+    }
+
+    public static void AddMask(string value)
+    {
+        Console.WriteLine($"::add-mask::{value}");
+    }
+
+    public static IDisposable StartStopCommand()
+    {
+        var endToken = Guid.NewGuid().ToString();
+        return new AreaCommand("stop-commands", endToken, endToken);
+    }
+
+    private class AreaCommand : IDisposable
+    {
+        private readonly string _end;
+
+        public AreaCommand(string start, string end, string title)
+        {
+            _end = end;
+            Console.WriteLine($"::{start}::{title}");
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine($"::{_end}::");
+        }
+    }
+}
---
+++ Actions.Utils/OutputParameters.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Actions.Utils;
+
+public class OutputParameters(IConfiguration configuration)
+{
+    private readonly string _gitHubOutput = configuration["GITHUB_OUTPUT"]!;
+
+    public Task Set(string name, string value)
+    {
+        return File.AppendAllLinesAsync(_gitHubOutput, [$"{name}={value}"]);
+    }
+}
